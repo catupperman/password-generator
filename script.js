3cf0c3b244f50c796f54592424890a1af24c9550
Catherine Upperman <catherineupperman@Catherines-MacBook-Air.local>
2021-06-18 09:26:41 -0400

    Putting some puzzle pieces down.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Array Options for the computer to choose from randomly.
 var specialChar = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//will the numbers need to be in a string?  Will this cause an error with Math.floor/random?
 
-//TO DO: When clicking  the button it generates a password, series of prompts pswd criteria
+//GOAL: When clicking password button it generates a password, series of prompts pswd criteria
+
+
 //TODO: Pswd criteria: Length of atleat 8 characters, no more than 128
 //TODO: Prompts for character type, lower, uppercase, numeric, and/or special characters
 
@@ -14,12 +18,21 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  //Math.floor(Math.random() * specialChar.length)
+  //Math.floor(Math.random() * upper.length)
+  //Math.floor(Math.random() * lower.length)
+  //Math.floor(Math.random() * numbers.length)
+
+
   passwordText.value = password;
 
 }
 
+//How to get it when clicking in the password space of the webpage?
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+window.confirm("Would you like to generate a password?")
+//if statement?
 
 
 //TODO: when answering  each prompt input should be validated and at least one character type should be selected
